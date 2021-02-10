9f73219e639f7881d8db274a1f4688d753f2bf99
StevenTheBearded <>
2021-02-10 18:12:56 -0500

    Initial commit

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,49 @@
-function nameMenuItem() {
-
+function nameMenuItem(x) {
+  return 'Delicious ' + x
 }
+
+function createMenuItem(x,y,z) {
+  var menuItem = {
+  name: x,
+  price: y,
+  type: z,
+  }
+  return menuItem
+}
+
+function addIngredients(x, ingredients) {
+ if (ingredients.includes(x)){
+  return ingredients
+ }else
+  ingredients.push(x)
+}
+
+function formatPrice(x) {
+  return '$' + x
+}
+
+function decreasePrice(x) {
+ return x - (x * .1)
+}
+
+function createRecipe(title, ingredients, type) {
+  return recipe = {
+  title,
+  ingredients,
+  type,
+  }
+}
+
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
--- src/order.js
+++ src/order.js
@@ -1,7 +1,32 @@
+function takeOrder(order,deliveryOrders) {
+  if(deliveryOrders.length < 3)
+  return deliveryOrders.push(order)
+  }
+
+function refundOrder(x,deliveryOrders) {
+  deliveryOrders.splice(x-1, 1)
+}
+
+function listItems(deliveryOrders) {
+  var orders = []
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    orders.push(deliveryOrders[i].item)
+  }
+return orders.join(", ")
+}
+
+
+function searchOrder(deliveryOrders, x) {
+  if (deliveryOrders.includes(x) === true) {
+    return true;
+  }else{
+    return false;
+  }
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
